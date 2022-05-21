8031a49c969c73cccb3a007b3b472d95a599feb2
Ilboudo Souleymane <ilboudosouleymane4@gmail.com>
2022-05-21 19:24:51 +0000

    fake drawer

--- components/Home.js
+++ components/Home.js
@@ -1,43 +1,172 @@
-import React from "react";
-import { FontAwesome } from "@expo/vector-icons";
-import { NativeBaseProvider } from "native-base";
-import { ImageBackground } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import {
-  Center,
-  HStack,
-  Image,
+  createDrawerNavigator,
+  DrawerContentScrollView,
+} from "@react-navigation/drawer";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  NativeBaseProvider,
+  Button,
   Box,
+  HamburgerIcon,
+  Pressable,
   Heading,
   VStack,
-  Button,
-  FormControl,
-  Input,
-  Link,
   Text,
-  Alert,
+  Center,
+  HStack,
+  Divider,
+  Icon,
 } from "native-base";
+const Drawer = createDrawerNavigator();
+function Component(props) {
+  return (
+    <Center>
+      <Text mt="12" fontSize="18">
+        This is {props.route.name} page.
+      </Text>
+    </Center>
+  );
+}
 
-const Home = ({ navigation }) => (
-  <NativeBaseProvider>
-    <Box bg="primary.800" h="100%">
-      <VStack space={5} alignItems="center">
-        <Box w="80%" h="10%" bg="indigo.300" rounded="md" shadow={3} />
-        <Button w="80%" h="25%" bg="indigo.300" rounded="md" shadow={3}>
-          <ImageBackground
-            source={require("../assets/img/1xbet.png")}
-            style={{ width: "100%", height: "100%" }}
-          >
-            <Heading color="white">
-              <FontAwesome name="money" size={20} />
-              DEPOT 1XBET
-            </Heading>
-          </ImageBackground>
-        </Button>
-        <Button w="80%" h="25%" bg="indigo.500" rounded="md" shadow={3} />
-        <Button w="80%" h="25%" bg="indigo.700" rounded="md" shadow={3} />
+const getIcon = (screenName) => {
+  switch (screenName) {
+    case "Inbox":
+      return "email";
+    case "Outbox":
+      return "send";
+    case "Favorites":
+      return "heart";
+    case "Archive":
+      return "archive";
+    case "Trash":
+      return "trash-can";
+    case "Spam":
+      return "alert-circle";
+    default:
+      return undefined;
+  }
+};
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props} safeArea>
+      <VStack space="6" my="2" mx="1">
+        <Box px="4">
+          <Text bold color="gray.700">
+            Mail
+          </Text>
+          <Text fontSize="14" mt="1" color="gray.500" fontWeight="500">
+            john_doe@gmail.com
+          </Text>
+        </Box>
+        <VStack divider={<Divider />} space="4">
+          <VStack space="3">
+            {props.state.routeNames.map((name, index) => (
+              <Pressable
+                px="5"
+                py="3"
+                rounded="md"
+                bg={
+                  index === props.state.index
+                    ? "rgba(6, 182, 212, 0.1)"
+                    : "transparent"
+                }
+                onPress={(event) => {
+                  props.navigation.navigate(name);
+                }}
+              >
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color={
+                      index === props.state.index ? "primary.500" : "gray.500"
+                    }
+                    size="5"
+                    as={<MaterialCommunityIcons name={getIcon(name)} />}
+                  />
+                  <Text
+                    fontWeight="500"
+                    color={
+                      index === props.state.index ? "primary.500" : "gray.700"
+                    }
+                  >
+                    {name}
+                  </Text>
+                </HStack>
+              </Pressable>
+            ))}
+          </VStack>
+          <VStack space="5">
+            <Text fontWeight="500" fontSize="14" px="5" color="gray.500">
+              Labels
+            </Text>
+            <VStack space="3">
+              <Pressable px="5" py="3">
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color="gray.500"
+                    size="5"
+                    as={<MaterialCommunityIcons name="bookmark" />}
+                  />
+                  <Text color="gray.700" fontWeight="500">
+                    Family
+                  </Text>
+                </HStack>
+              </Pressable>
+              <Pressable px="5" py="2">
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color="gray.500"
+                    size="5"
+                    as={<MaterialCommunityIcons name="bookmark" />}
+                  />
+                  <Text color="gray.700" fontWeight="500">
+                    Friends
+                  </Text>
+                </HStack>
+              </Pressable>
+              <Pressable px="5" py="3">
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color="gray.500"
+                    size="5"
+                    as={<MaterialCommunityIcons name="bookmark" />}
+                  />
+                  <Text fontWeight="500" color="gray.700">
+                    Work
+                  </Text>
+                </HStack>
+              </Pressable>
+            </VStack>
+          </VStack>
+        </VStack>
       </VStack>
+    </DrawerContentScrollView>
+  );
+}
+function MyDrawer() {
+  return (
+    <Box safeArea flex={1}>
+      <Drawer.Navigator
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
+        <Drawer.Screen name="Inbox" component={Component} />
+        <Drawer.Screen name="Outbox" component={Component} />
+        <Drawer.Screen name="Favorites" component={Component} />
+        <Drawer.Screen name="Archive" component={Component} />
+        <Drawer.Screen name="Trash" component={Component} />
+        <Drawer.Screen name="Spam" component={Component} />
+      </Drawer.Navigator>
     </Box>
-  </NativeBaseProvider>
-);
-
-export default Home;
+  );
+}
+export default function Example() {
+  return (
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <MyDrawer />
+      </NativeBaseProvider>
+    </NavigationContainer>
+  );
+}
