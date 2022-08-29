6be9791400e2cbfea457f9a3f6d4749dc33c137f
Souleymane <ilboudosouleymane4@gmail.com>
2022-08-29 08:23:35 +0000

    carte add

--- View/Compte.js
+++ View/Compte.js
@@ -1,50 +1,118 @@
-import React from 'react';
-import { VStack, Box,Text, Center, Input } from 'native-base';
-import CreditCardDisplay from 'react-native-credit-card-display';
-import Swiper from 'react-native-swiper';
+import React from "react";
+import {
+  VStack,
+  Box,
+  Text,
+  Center,
+  Input,
+  HStack,
+  Stack,
+  AspectRatio,
+  Heading,
+} from "native-base";
+import CreditCardDisplay from "react-native-credit-card-display";
 
-const Compte =({navigation}) => {
+import Swiper from "react-native-swiper";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-    const [card, setCard] = React.useState({
-        cardNumber: '',
-        expirationDate: '',
-        cvv: '',
-        cardHolderName: '',
-    });
-    const [ flip, setFlip ] = React.useState(false);
-    
+const Compte = ({ navigation }) => {
+  const [card, setCard] = React.useState({
+    cardNumber: "",
+    expirationDate: "",
+    cvv: "",
+    cardHolderName: "",
+  });
+  const [flip, setFlip] = React.useState(false);
 
-    return (
-        <VStack>
-            <Box width={'100%'}>
-                <Center >
-                <CreditCardDisplay
-                    number={4242424242424242}
-                    cvc={123}
-                    expiration="04/21"
-                    name="John J. Doe"
-                    fontSize={20}
-                    flipped={flip}
-
-                    
-                    
-                />
-               
-                </Center>
-                <Center>
-                    <Swiper>
-                        <Box>
-                            <Input placeholder="Card Number" onChangeText={(text) => setCard({...card, cardNumber: text})} />
-                        </Box>
-                        <Box>
-                            <Input placeholder="Expiration Date" onChangeText={(text) => setCard({...card, expirationDate: text})} />
-                        </Box>
-                    </Swiper>
-                </Center>
+  return (
+    <KeyboardAwareScrollView>
+      <VStack>
+        <Box width={"100%"}>
+          <Center>
+            <Box alignItems="center">
+              <Box
+                maxW="80"
+                rounded="lg"
+                overflow="hidden"
+                borderColor="coolGray.200"
+                borderWidth="1"
+                _dark={{
+                  borderColor: "coolGray.600",
+                  backgroundColor: "gray.700",
+                }}
+                _web={{
+                  shadow: 2,
+                  borderWidth: 0,
+                }}
+                _light={{
+                  backgroundColor: "gray.50",
+                }}
+              >
+                <Box>
+                  <AspectRatio w="100%" ratio={16 / 9}>
+                    <Box width={"100%"}>
+                      <Center>
+                        <CreditCardDisplay
+                          number={`${card.cardNumber}`}
+                          cvc={`${card.cvv}`}
+                          expiration={`${card.expirationDate}`}
+                          name={`${card.cardHolderName}`}
+                          fontSize={20}
+                          flipped={flip}
+                          width={300}
+                        />
+                      </Center>
+                    </Box>
+                  </AspectRatio>
+                </Box>
+                <Stack p="4" space={3}>
+                  <Stack space={2}>
+                    <Heading size="md" ml="-1">
+                      <Text>INFO DE LA CARTE</Text>
+                    </Heading>
+                    <HStack space={2} mt={2} alignItems="center">
+                      <Swiper style={{ height: 120 }}>
+                        <Input
+                          placeholder="Nom du titulaire"
+                          onChangeText={(text) =>
+                            setCard({ ...card, cardHolderName: text })
+                          }
+                        />
+                        <Input
+                          placeholder="Numéro de carte"
+                          onChangeText={(text) =>
+                            setCard({ ...card, cardNumber: text })
+                          }
+                        />
+                        <Input
+                          placeholder="Date d'expiration"
+                          onChangeText={(text) =>
+                            setCard({ ...card, expirationDate: text })
+                          }
+                        />
+                        <Input
+                          placeholder="Code de sécurité(cvc)"
+                          onChangeText={(text) =>
+                            setCard({ ...card, cvv: text })
+                          }
+                          onBlur={() => setFlip(true)}
+                        />
+                      </Swiper>
+                    </HStack>
+                  </Stack>
+                  <HStack
+                    alignItems="center"
+                    space={4}
+                    justifyContent="space-between"
+                  ></HStack>
+                </Stack>
+              </Box>
             </Box>
-        </VStack>
-    );
-}
+          </Center>
+        </Box>
+      </VStack>
+    </KeyboardAwareScrollView>
+  );
+};
 
-    export default Compte;
-
+export default Compte;
--- View/Signup.js
+++ View/Signup.js
@@ -5,6 +5,7 @@ import { useForm, Controller } from "react-hook-form";
 import { FontAwesome } from "@expo/vector-icons";
 import { signupUsers } from "../api/request";
 import Loader from "../components/Loader";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import {
   NativeBaseProvider,
   Center,
@@ -25,7 +26,6 @@ export const shwoToast = (message) => {
   ToastAndroid.show(message, ToastAndroid.SHORT);
 };
 
-
 const Signup = ({ navigation }) => {
   const [password, setPassword] = useState("");
   const [isLoading, setLoading] = useState(false);
@@ -39,28 +39,25 @@ const Signup = ({ navigation }) => {
       password: "",
     },
   });
- 
+
   const signup = (data) => {
     setLoading(true);
-    signupUsers(data).then((res) => {
+    signupUsers(data)
+      .then((res) => {
         setLoading(false);
         shwoToast(res.data?.message);
         code = res.data?.data?.code_verified;
-        navigation.navigate("verifyEmail", { email: data.email , code: code});
+        navigation.navigate("verifyEmail", { email: data.email, code: code });
         shwoToast(res.data.message);
-      }
-    ).catch((err) => {
-      setLoading(false);
-      shwoToast(err.message);
-    });
-  }
-
-    
-    
-
+      })
+      .catch((err) => {
+        setLoading(false);
+        shwoToast(err.message);
+      });
+  };
 
   const onSubmit = (data) => {
-   signup(data);
+    signup(data);
   };
   if (isLoading) {
     return (
@@ -71,204 +68,210 @@ const Signup = ({ navigation }) => {
   } else {
     return (
       <NativeBaseProvider>
-        <Center w="100%" h="90%">
-          <HStack justifyContent="center">
-            <Image source={require("../assets/img/logo.png")} alt="logo" />
-          </HStack>
-          <Box safeArea p="2" w="90%" maxW="290" py="8">
-            <Heading
-              size="lg"
-              color="coolGray.800"
-              _dark={{
-                color: "warmGray.50",
-              }}
-              fontWeight="semibold"
-            >
-              Vous n'avez pas de compte!
-            </Heading>
-            <Heading
-              mt="1"
-              color="coolGray.600"
-              _dark={{
-                color: "warmGray.200",
-              }}
-              fontWeight="medium"
-              size="xs"
-            >
-              Inscrivez-vous pour continuer
-            </Heading>
-            <VStack space={3} mt="5">
-              <FormControl isRequired>
-                <Controller
-                  control={control}
-                  name="lastname"
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <Input
-                      onChangeText={onChange}
-                      value={value}
-                      placeholder="Nom"
-                      onBlur={onBlur}
-                      InputLeftElement={
-                        <FontAwesome
-                          name="user"
-                          size={20}
-                          style={{ marginLeft: 20 }}
-                        />
-                      }
-                    />
-                  )}
-                  rules={{
-                    required: "Champs obligatoire",
-                  }}
-                  defaultValue=""
-                />
-                {errors.lastname && (
-                  <Text style={{ color: "red" }}>{errors.lastname.message}</Text>
-                )}
-              </FormControl>
-              <FormControl isRequired>
-                <Controller
-                  control={control}
-                  name="firstname"
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <Input
-                      onChangeText={onChange}
-                      value={value}
-                      placeholder="Prenom"
-                      onBlur={onBlur}
-                      InputLeftElement={
-                        <FontAwesome
-                          name="user"
-                          size={20}
-                          style={{ marginLeft: 20 }}
-                        />
-                      }
-                    />
-                  )}
-                  rules={{
-                    required: "Champs obligatoire",
-                  }}
-                  defaultValue=""
-                />
-                {errors.firstname && (
-                  <Text style={{ color: "red" }}>{errors.firstname.message}</Text>
-                )}
-              </FormControl>
-              <FormControl isRequired>
-                <Controller
-                  control={control}
-                  name="email"
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <Input
-                      onChangeText={onChange}
-                      value={value}
-                      placeholder="Email"
-                      onBlur={onBlur}
-                      InputLeftElement={
-                        <FontAwesome
-                          name="envelope"
-                          size={20}
-                          style={{ marginLeft: 20 }}
-                        />
-                      }
-                    />
-                  )}
-                  rules={{
-                    required: "Champs obligatoire",
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                      message: "Email invalide",
-                    },
-                  }}
-                  defaultValue=""
-                />
-                {errors.email && (
-                  <Text style={{ color: "red" }}>{errors.email.message}</Text>
-                )}
-              </FormControl>
-              <FormControl isRequired>
-                <Controller
-                  control={control}
-                  name="password"
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <Input
-                      type="password"
-                      onChangeText={onChange}
-                      onChange={(e) => {
-                        setPassword(e.nativeEvent.text);
-                        onChange(e);
-                      }}
-                      value={value}
-                      onBlur={onBlur}
-                      InputLeftElement={
-                        <FontAwesome
-                          name="lock"
-                          size={20}
-                          style={{ marginLeft: 20 }}
-                        />
-                      }
-                      placeholder="Mot de passe"
-                    />
-                  )}
-                  rules={{
-                    required: "Champs obligatoire",
-                    pattern: {
-                      value: /^[a-zA-Z0-9]{6,}$/i,
-                      message: "Mot de passe trop court",
-                    },
-                  }}
-                  defaultValue=""
-                />
-                {errors.password && (
-                  <Text style={{ color: "red" }}>
-                    {errors.password.message}
-                  </Text>
-                )}
-              </FormControl>
-              <FormControl isRequired>
-                <Controller
-                  control={control}
-                  name="password_confirmation"
-                  render={({ field: { onChange, onBlur, value } }) => (
-                    <Input
-                      type="password"
-                      onChangeText={onChange}
-                      value={value}
-                      onBlur={onBlur}
-                      InputLeftElement={
-                        <FontAwesome
-                          name="lock"
-                          size={20}
-                          style={{ marginLeft: 20 }}
-                        />
-                      }
-                      placeholder="Retapez votre mot de passe"
-                    />
-                  )}
-                  rules={{
-                    required: "Champs obligatoire",
-                    //pattern for password confirmation
-                    validate: (value) =>
-                      value === password ||
-                      "Les mots de passe ne correspondent pas",
-                  }}
-                />
-                {errors.password_confirmation && (
-                  <Text style={{ color: "red" }}>
-                    {errors.password_confirmation.message}
-                  </Text>
-                )}
-              </FormControl>
-              <Button
-                mt="2"
-                colorScheme="indigo"
-                onPress={handleSubmit(onSubmit)}
+        <KeyboardAwareScrollView>
+          <Center w="100%" h="90%" mt={10}>
+            <HStack justifyContent="center">
+              <Image source={require("../assets/img/logo.png")} alt="logo" />
+            </HStack>
+            <Box safeArea p="2" w="90%" maxW="290" py="8">
+              <Heading
+                size="lg"
+                color="coolGray.800"
+                _dark={{
+                  color: "warmGray.50",
+                }}
+                fontWeight="semibold"
               >
-                Inscription
-              </Button>
-            </VStack>
-          </Box>
-        </Center>
+                Vous n'avez pas de compte!
+              </Heading>
+              <Heading
+                mt="1"
+                color="coolGray.600"
+                _dark={{
+                  color: "warmGray.200",
+                }}
+                fontWeight="medium"
+                size="xs"
+              >
+                Inscrivez-vous pour continuer
+              </Heading>
+              <VStack space={3} mt="5">
+                <FormControl isRequired>
+                  <Controller
+                    control={control}
+                    name="lastname"
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <Input
+                        onChangeText={onChange}
+                        value={value}
+                        placeholder="Nom"
+                        onBlur={onBlur}
+                        InputLeftElement={
+                          <FontAwesome
+                            name="user"
+                            size={20}
+                            style={{ marginLeft: 20 }}
+                          />
+                        }
+                      />
+                    )}
+                    rules={{
+                      required: "Champs obligatoire",
+                    }}
+                    defaultValue=""
+                  />
+                  {errors.lastname && (
+                    <Text style={{ color: "red" }}>
+                      {errors.lastname.message}
+                    </Text>
+                  )}
+                </FormControl>
+                <FormControl isRequired>
+                  <Controller
+                    control={control}
+                    name="firstname"
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <Input
+                        onChangeText={onChange}
+                        value={value}
+                        placeholder="Prenom"
+                        onBlur={onBlur}
+                        InputLeftElement={
+                          <FontAwesome
+                            name="user"
+                            size={20}
+                            style={{ marginLeft: 20 }}
+                          />
+                        }
+                      />
+                    )}
+                    rules={{
+                      required: "Champs obligatoire",
+                    }}
+                    defaultValue=""
+                  />
+                  {errors.firstname && (
+                    <Text style={{ color: "red" }}>
+                      {errors.firstname.message}
+                    </Text>
+                  )}
+                </FormControl>
+                <FormControl isRequired>
+                  <Controller
+                    control={control}
+                    name="email"
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <Input
+                        onChangeText={onChange}
+                        value={value}
+                        placeholder="Email"
+                        onBlur={onBlur}
+                        InputLeftElement={
+                          <FontAwesome
+                            name="envelope"
+                            size={20}
+                            style={{ marginLeft: 20 }}
+                          />
+                        }
+                      />
+                    )}
+                    rules={{
+                      required: "Champs obligatoire",
+                      pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: "Email invalide",
+                      },
+                    }}
+                    defaultValue=""
+                  />
+                  {errors.email && (
+                    <Text style={{ color: "red" }}>{errors.email.message}</Text>
+                  )}
+                </FormControl>
+                <FormControl isRequired>
+                  <Controller
+                    control={control}
+                    name="password"
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <Input
+                        type="password"
+                        onChangeText={onChange}
+                        onChange={(e) => {
+                          setPassword(e.nativeEvent.text);
+                          onChange(e);
+                        }}
+                        value={value}
+                        onBlur={onBlur}
+                        InputLeftElement={
+                          <FontAwesome
+                            name="lock"
+                            size={20}
+                            style={{ marginLeft: 20 }}
+                          />
+                        }
+                        placeholder="Mot de passe"
+                      />
+                    )}
+                    rules={{
+                      required: "Champs obligatoire",
+                      pattern: {
+                        value: /^[a-zA-Z0-9]{6,}$/i,
+                        message: "Mot de passe trop court",
+                      },
+                    }}
+                    defaultValue=""
+                  />
+                  {errors.password && (
+                    <Text style={{ color: "red" }}>
+                      {errors.password.message}
+                    </Text>
+                  )}
+                </FormControl>
+                <FormControl isRequired>
+                  <Controller
+                    control={control}
+                    name="password_confirmation"
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <Input
+                        type="password"
+                        onChangeText={onChange}
+                        value={value}
+                        onBlur={onBlur}
+                        InputLeftElement={
+                          <FontAwesome
+                            name="lock"
+                            size={20}
+                            style={{ marginLeft: 20 }}
+                          />
+                        }
+                        placeholder="Retapez votre mot de passe"
+                      />
+                    )}
+                    rules={{
+                      required: "Champs obligatoire",
+                      //pattern for password confirmation
+                      validate: (value) =>
+                        value === password ||
+                        "Les mots de passe ne correspondent pas",
+                    }}
+                  />
+                  {errors.password_confirmation && (
+                    <Text style={{ color: "red" }}>
+                      {errors.password_confirmation.message}
+                    </Text>
+                  )}
+                </FormControl>
+                <Button
+                  mt="2"
+                  colorScheme="indigo"
+                  onPress={handleSubmit(onSubmit)}
+                >
+                  Inscription
+                </Button>
+              </VStack>
+            </Box>
+          </Center>
+        </KeyboardAwareScrollView>
       </NativeBaseProvider>
     );
   }
